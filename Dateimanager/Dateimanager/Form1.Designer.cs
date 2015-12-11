d7991001aa2c32aefa81e32cbccd4f19eb16005a
Christoph Schimpf <christoph@r-schimpf.de>
2015-12-11 20:05:26 +0100

    Test

---
+++ Dateimanager/Dateimanager/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace Dateimanager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.bindingNavigator1 = new System.Windows.Forms.BindingNavigator(this.components);
+            this.bindingNavigatorMovePreviousItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.bindingNavigatorSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorMoveNextItem = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
+            this.neuToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.ausschneidenToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.kopierenToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.einfügenToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorDeleteItem = new System.Windows.Forms.ToolStripButton();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingNavigator1)).BeginInit();
+            this.bindingNavigator1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(194, 483);
+            this.treeView1.TabIndex = 3;
+            this.treeView1.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView1_BeforeExpand);
+            this.treeView1.NodeMouseDoubleClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseDoubleClick);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.treeView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.AutoScroll = true;
+            this.splitContainer1.Panel2.Controls.Add(this.bindingNavigator1);
+            this.splitContainer1.Panel2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.splitContainer1.Size = new System.Drawing.Size(839, 483);
+            this.splitContainer1.SplitterDistance = 194;
+            this.splitContainer1.TabIndex = 4;
+            // 
+            // bindingNavigator1
+            // 
+            this.bindingNavigator1.AddNewItem = null;
+            this.bindingNavigator1.CountItem = null;
+            this.bindingNavigator1.DeleteItem = null;
+            this.bindingNavigator1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bindingNavigatorMovePreviousItem,
+            this.bindingNavigatorSeparator,
+            this.toolStripLabel1,
+            this.bindingNavigatorSeparator1,
+            this.bindingNavigatorMoveNextItem,
+            this.toolStripSeparator1,
+            this.toolStripComboBox1,
+            this.bindingNavigatorSeparator2,
+            this.bindingNavigatorAddNewItem,
+            this.neuToolStripButton,
+            this.ausschneidenToolStripButton,
+            this.kopierenToolStripButton,
+            this.einfügenToolStripButton,
+            this.bindingNavigatorDeleteItem});
+            this.bindingNavigator1.Location = new System.Drawing.Point(0, 0);
+            this.bindingNavigator1.MoveFirstItem = null;
+            this.bindingNavigator1.MoveLastItem = null;
+            this.bindingNavigator1.MoveNextItem = null;
+            this.bindingNavigator1.MovePreviousItem = null;
+            this.bindingNavigator1.Name = "bindingNavigator1";
+            this.bindingNavigator1.PositionItem = null;
+            this.bindingNavigator1.Size = new System.Drawing.Size(641, 25);
+            this.bindingNavigator1.TabIndex = 0;
+            this.bindingNavigator1.Text = "bindingNavigator1";
+            // 
+            // bindingNavigatorMovePreviousItem
+            // 
+            this.bindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMovePreviousItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMovePreviousItem.Image")));
+            this.bindingNavigatorMovePreviousItem.Name = "bindingNavigatorMovePreviousItem";
+            this.bindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMovePreviousItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMovePreviousItem.Text = "Vorherige verschieben";
+            this.bindingNavigatorMovePreviousItem.Click += new System.EventHandler(this.bindingNavigatorMovePreviousItem_Click_1);
+            // 
+            // bindingNavigatorSeparator
+            // 
+            this.bindingNavigatorSeparator.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.AutoSize = false;
+            this.toolStripLabel1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(151, 22);
+            this.toolStripLabel1.Text = "C:\\";
+            // 
+            // bindingNavigatorSeparator1
+            // 
+            this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator1";
+            this.bindingNavigatorSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // bindingNavigatorMoveNextItem
+            // 
+            this.bindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveNextItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveNextItem.Image")));
+            this.bindingNavigatorMoveNextItem.Name = "bindingNavigatorMoveNextItem";
+            this.bindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveNextItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMoveNextItem.Text = "Nächste verschieben";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(121, 25);
+            this.toolStripComboBox1.Text = "Pfad";
+            // 
+            // bindingNavigatorSeparator2
+            // 
+            this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator2";
+            this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // bindingNavigatorAddNewItem
+            // 
+            this.bindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
+            this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
+            this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorAddNewItem.Text = "Neuer Ordner";
+            this.bindingNavigatorAddNewItem.Click += new System.EventHandler(this.bindingNavigatorAddNewItem_Click);
+            // 
+            // neuToolStripButton
+            // 
+            this.neuToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.neuToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("neuToolStripButton.Image")));
+            this.neuToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.neuToolStripButton.Name = "neuToolStripButton";
+            this.neuToolStripButton.Size = new System.Drawing.Size(23, 22);
+            this.neuToolStripButton.Text = "&Neu";
+            this.neuToolStripButton.Click += new System.EventHandler(this.neuToolStripButton_Click);
+            // 
+            // ausschneidenToolStripButton
+            // 
+            this.ausschneidenToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.ausschneidenToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("ausschneidenToolStripButton.Image")));
+            this.ausschneidenToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ausschneidenToolStripButton.Name = "ausschneidenToolStripButton";
+            this.ausschneidenToolStripButton.Size = new System.Drawing.Size(23, 22);
+            this.ausschneidenToolStripButton.Text = "&Ausschneiden";
+            this.ausschneidenToolStripButton.Click += new System.EventHandler(this.ausschneidenToolStripButton_Click);
+            // 
+            // kopierenToolStripButton
+            // 
+            this.kopierenToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.kopierenToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("kopierenToolStripButton.Image")));
+            this.kopierenToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.kopierenToolStripButton.Name = "kopierenToolStripButton";
+            this.kopierenToolStripButton.Size = new System.Drawing.Size(23, 22);
+            this.kopierenToolStripButton.Text = "&Kopieren";
+            this.kopierenToolStripButton.Click += new System.EventHandler(this.kopierenToolStripButton_Click);
+            // 
+            // einfügenToolStripButton
+            // 
+            this.einfügenToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.einfügenToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("einfügenToolStripButton.Image")));
+            this.einfügenToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.einfügenToolStripButton.Name = "einfügenToolStripButton";
+            this.einfügenToolStripButton.Size = new System.Drawing.Size(23, 22);
+            this.einfügenToolStripButton.Text = "&Einfügen";
+            this.einfügenToolStripButton.Click += new System.EventHandler(this.einfügenToolStripButton_Click);
+            // 
+            // bindingNavigatorDeleteItem
+            // 
+            this.bindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
+            this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
+            this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorDeleteItem.Text = "Löschen";
+            this.bindingNavigatorDeleteItem.Click += new System.EventHandler(this.bindingNavigatorDeleteItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(839, 483);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Dateimanager DM";
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingNavigator1)).EndInit();
+            this.bindingNavigator1.ResumeLayout(false);
+            this.bindingNavigator1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.BindingNavigator bindingNavigator1;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator1;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator2;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorAddNewItem;
+        private System.Windows.Forms.ToolStripButton neuToolStripButton;
+        private System.Windows.Forms.ToolStripButton ausschneidenToolStripButton;
+        private System.Windows.Forms.ToolStripButton kopierenToolStripButton;
+        private System.Windows.Forms.ToolStripButton einfügenToolStripButton;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMovePreviousItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveNextItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorDeleteItem;
+    }
+}
+
---
+++ Dateimanager/Dateimanager/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Dateimanager
+{
+    public partial class Form1 : Form
+    {
+        Transfer transfer = new Transfer();
+
+
+        List<Button> Buttons = new List<Button>();
+
+        int x = 0;
+        int y = 0;
+        int i = 0;
+        string PrevPfad = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            showButton();
+            RefreshTreeView();
+        }
+
+        private void RefreshTreeView()
+        {
+            foreach (string drives in Directory.GetLogicalDrives())
+            {
+                TreeNode node = new TreeNode();
+                node.Name = drives;
+                node.Text = drives;
+                treeView1.Nodes.Add(node);
+                RefreshTreeView(node);
+            }
+        }
+
+        private void RefreshTreeView(TreeNode node)
+        {
+            try
+            {
+                foreach (string dir in Directory.GetDirectories(node.Name))
+                {
+                    TreeNode TNode = new TreeNode();
+                    TNode.Name = dir;
+                    TNode.Text = dir.Split('\\')[dir.Split('\\').Length - 1];
+                    node.Nodes.Add(TNode);
+                }
+            }
+            catch { }
+
+        }
+
+        private void treeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            foreach (TreeNode node in e.Node.Nodes)
+            {
+                RefreshTreeView(node);
+            }
+        }
+
+        private void treeView1_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            toolStripLabel1.Text = e.Node.Name;
+            showButton();
+        }
+
+        private void AddButton(string name, string text, Color color)
+        {
+            if (x + 85 > splitContainer1.Panel2.Width)
+            {
+                y += 85;
+                x = 0;
+            }
+
+            // 
+            // button(i)
+            // 
+            Buttons.Add(new Button());
+            Buttons[i].Location = new System.Drawing.Point(3 + x, 28 + y);
+            Buttons[i].Name = name;
+            Buttons[i].Size = new System.Drawing.Size(75, 75);
+            Buttons[i].TabIndex = 1;
+            Buttons[i].Text = text;
+            Buttons[i].UseVisualStyleBackColor = true;
+            Buttons[i].BackColor = color;
+            Buttons[i].ForeColor = Color.Black;
+            Buttons[i].MouseClick += new MouseEventHandler(Click);
+            //Buttons[i].MouseDoubleClick += new MouseEventHandler(DoubleClick);
+            splitContainer1.Panel2.Controls.Add(Buttons[i]);
+            x = x + 85;
+            i++;
+        }
+
+        private void showButton()
+        {
+            foreach (Button b in Buttons)
+            {
+                b.Dispose();
+            }
+
+            x = 0;
+            y = 0;
+            i = 0;
+
+            Buttons.Clear();
+
+            foreach (String d in Directory.GetDirectories(toolStripLabel1.Text))
+            {
+                AddButton(d, d.Split('\\')[d.Split('\\').Length - 1], Color.Orange);
+            }
+
+            foreach (String d in Directory.GetFiles(toolStripLabel1.Text))
+            {
+                AddButton(d, d.Split('\\')[d.Split('\\').Length - 1], Color.YellowGreen);
+            }
+
+        }
+
+        private new void Click(object sender, MouseEventArgs e)
+        {
+            MouseEventArgs me = e;
+
+            Button bs = (Button)sender;
+            if (bs.ForeColor == Color.Black)
+                bs.ForeColor = Color.White;
+
+            else
+                bs.ForeColor = Color.Black;
+
+        }
+
+        private new void DoubleClick(object sender, EventArgs e)
+        {
+            MessageBox.Show("test");
+            Button bs = (Button)sender;
+            toolStripLabel1.Text = bs.Name;
+            showButton();
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            showButton();
+        }
+
+        private void bindingNavigatorAddNewItem_Click(object sender, EventArgs e)
+        {
+            //AddButton("Pfad/Neuer Ordner", "Neuer Ordner");
+            transfer.CreateFolder(toolStripLabel1.Text, "Neuer Ordner");
+            showButton();
+
+        }
+
+        private void bindingNavigatorMovePreviousItem_Click_1(object sender, EventArgs e)
+        {
+            PrevPfad = Path.GetDirectoryName(toolStripLabel1.Text);
+            if (PrevPfad == null)
+            {
+                toolStripLabel1.Text = @"C:\";
+            }
+            else
+            {
+                toolStripLabel1.Text = PrevPfad;
+            }
+            showButton();
+        }
+
+        private void neuToolStripButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ausschneidenToolStripButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void kopierenToolStripButton_Click(object sender, EventArgs e)
+        {
+            System.Collections.Specialized.StringCollection sc = new System.Collections.Specialized.StringCollection();
+            foreach (Button b in Buttons)
+            {
+                if (b.ForeColor == Color.White)
+                {
+                    sc.Add(b.Name);
+                }
+            }
+            Clipboard.SetFileDropList(sc);
+        }
+
+        private void einfügenToolStripButton_Click(object sender, EventArgs e)
+        {
+            System.Collections.Specialized.StringCollection sc = Clipboard.GetFileDropList();
+            foreach (string b in sc)
+            {
+                Transfer t = new Transfer();
+                t.Copy(b, toolStripLabel1.Text);
+            }
+
+            showButton();
+
+        }
+
+        private void bindingNavigatorDeleteItem_Click(object sender, EventArgs e)
+        {
+            foreach (Button b in Buttons)
+            {
+                if (b.ForeColor == Color.White)
+                {
+                    Transfer t = new Transfer();
+                    t.Remove(b.Name);
+                }
+            }
+            showButton();
+        }
+    }
+}
